0fa7ae555ac6885e14d4c34f6b6ae532ed4e1570
Ramil Nugmanov <stsouko@live.ru>
2017-03-29 22:34:54 +0300

    init and config fixed. need merge CLI from MARS and PGAtools.

--- CGRdb/config.py
+++ CGRdb/config.py
@@ -21,44 +21,13 @@
 from os.path import join, exists, expanduser, dirname
 
 
-UPLOAD_PATH = 'upload'
-MAX_UPLOAD_SIZE = 16 * 1024 * 1024
-IMAGES_ROOT = join(UPLOAD_PATH, 'images')
-RESIZE_URL = '/static/images'
-PORTAL_NON_ROOT = ''
-SECRET_KEY = 'development key'
-YANDEX_METRIKA = None
 DEBUG = False
-
-LAB_NAME = 'Kazan Chemoinformatics and Molecular Modeling Laboratory'
-LAB_SHORT = 'CIMM'
-BLOG_POSTS_PER_PAGE = 10
-SCOPUS_API_KEY = None
-SCOPUS_TTL = 86400 * 7
-
-SMPT_HOST = None
-SMTP_PORT = None
-SMTP_LOGIN = None
-SMTP_PASSWORD = None
-SMTP_MAIL = None
-
-MAIL_INKEY = None
-MAIL_SIGNER = None
 
 DB_USER = None
 DB_PASS = None
 DB_HOST = None
 DB_NAME = None
-DB_MAIN = None
-DB_PRED = None
 DB_DATA = None
-
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_PASSWORD = None
-REDIS_TTL = 86400
-REDIS_JOB_TIMEOUT = 3600
-REDIS_MAIL = 'mail'
 
 FP_SIZE = 12
 FP_ACTIVE_BITS = 2
@@ -74,11 +43,7 @@ DATA_ISOTOPE = True
 DATA_STEREO = True
 
 
-config_list = ('UPLOAD_PATH', 'PORTAL_NON_ROOT', 'SECRET_KEY', 'RESIZE_URL', 'MAX_UPLOAD_SIZE', 'IMAGES_ROOT',
-               'DB_USER', 'DB_PASS', 'DB_HOST', 'DB_NAME', 'DB_MAIN', 'DB_PRED', 'DB_DATA', 'YANDEX_METRIKA',
-               'REDIS_HOST', 'REDIS_PORT', 'REDIS_PASSWORD', 'REDIS_TTL', 'REDIS_JOB_TIMEOUT', 'REDIS_MAIL',
-               'LAB_NAME', 'LAB_SHORT', 'BLOG_POSTS_PER_PAGE', 'SCOPUS_API_KEY', 'SCOPUS_TTL',
-               'SMPT_HOST', 'SMTP_PORT', 'SMTP_LOGIN', 'SMTP_PASSWORD', 'SMTP_MAIL', 'MAIL_INKEY', 'MAIL_SIGNER',
+config_list = ('DB_USER', 'DB_PASS', 'DB_HOST', 'DB_NAME', 'DB_MAIN', 'DB_PRED', 'DB_DATA',
                'FP_SIZE', 'FP_ACTIVE_BITS', 'FRAGMENTOR_VERSION', 'DATA_ISOTOPE', 'DATA_STEREO',
                'FRAGMENT_TYPE_CGR', 'FRAGMENT_MIN_CGR', 'FRAGMENT_MAX_CGR', 'FRAGMENT_DYNBOND_CGR',
                'FRAGMENT_TYPE_MOL', 'FRAGMENT_MIN_MOL', 'FRAGMENT_MAX_MOL')
@@ -86,7 +51,7 @@ config_list = ('UPLOAD_PATH', 'PORTAL_NON_ROOT', 'SECRET_KEY', 'RESIZE_URL', 'MA
 config_load_list = ['DEBUG']
 config_load_list.extend(config_list)
 
-config_dirs = [join(x, '.MWUI.ini') for x in (dirname(__file__), expanduser('~'), '/etc')]
+config_dirs = [join(x, '.CGRdb.ini') for x in (dirname(__file__), expanduser('~'), '/etc')]
 
 if not any(exists(x) for x in config_dirs):
     with open(config_dirs[1], 'w') as f:
---
+++ README.md
@@ -0,0 +1,7 @@
+**CGRdb** - DataBase Management system for chemical data
+
+INSTALL
+=======
+
+    pip install -U git+https://github.com/stsouko/CGRdb.git@master#egg=MWUI --process-dependency-links --allow-all-external
+
