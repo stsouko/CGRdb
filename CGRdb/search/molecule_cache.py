3f70c5964d5582ae0c3ea8e2a6bed6964f05c068
SZabirov <zab.sal42@gmail.com>
2018-11-27 21:34:01 +0300

    Cache expiration on item deleting fix

--- CGRdb/search/molecule_cache.py
+++ CGRdb/search/molecule_cache.py
@@ -14,6 +14,7 @@ class MoleculeCache(MutableMapping):
         self._expiration[datetime.now()] = k
 
     def __delitem__(self, k) -> None:
+        del self._expiration[list(self._expiration)[list(self._expiration.values()).index(k)]]
         del self._dict[k]
 
     def __getitem__(self, k: str):
@@ -27,7 +28,7 @@ class MoleculeCache(MutableMapping):
 
     def _clean_old(self):
         time_limit = datetime.now() - timedelta(days=1)
-        for time in list(self._expiration.keys()):
+        for time in list(self._expiration):
             if time < time_limit:
                 del self._dict[self._expiration[time]]
                 del self._expiration[time]
