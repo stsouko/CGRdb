76e219bccf0a6a127ab32cb9ad0b705c2f2f948b
Ramil Nugmanov <nougmanoff@protonmail.com>
2019-12-20 21:42:12 +0300

    Searches (#24)
    * searches of reactions by molecule done

--- CGRdb/sql/__init__.py
+++ CGRdb/sql/__init__.py
@@ -76,5 +76,6 @@ search_similar_molecules = load_sql('similar_molecule.sql')
 search_similar_reactions = load_sql('similar_reaction.sql')
 search_substructure_fingerprint_molecule = load_sql('substructure_fingerprint_molecule.sql')
 search_similar_fingerprint_molecule = load_sql('similar_fingerprint_molecule.sql')
+search_reactions_by_molecule = load_sql('reaction_by_molecule.sql')
 
 merge_molecules = load_sql('merge_molecules.sql')
